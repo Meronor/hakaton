615a8ff1c5dbc6fee470bfd4a64449e120b835c0
Boris <139627031+Meronor@users.noreply.github.com>
2025-04-09 04:17:01 +0200

    добавил текст

--- single_bot_hakaton/core_s/handlers/basic.py
+++ single_bot_hakaton/core_s/handlers/basic.py
@@ -6,40 +6,56 @@ from single_bot_hakaton.core_s.keyboards.inline import inline_yn
 
 f = False
 
+
 async def get_start(message: Message, bot: Bot):
-    await message.answer('*Приветствие*', reply_markup=reply_main())
-    # with open('123.png', 'rb') as photo_file:
-    #    await bot.send_photo(chat_id=message.chat.id, photo=photo_file)
-    await bot.send_sticker(
-        chat_id=message.chat.id,
-        sticker="CAACAgIAAxkBAAIK82fzu-tM_sknVm6-M8BDIbyir8beAAIzbgACcAKhSzqBlL5pI8reNgQ")
+    await message.answer('''Привет, я бот, который поможет тебе разобраться с документами, 
+                                получить поддержку или помощь от менеджера команды "Лидеры России- Москва", 
+                                а также принять заявку на опубликования искусства на сайте все СВОи! 
+                                                    Что делаете сделать?''', reply_markup=reply_main())
+    await bot.send_sticker(chat_id=message.chat.id,
+                           sticker="CAACAgIAAxkBAAIK82fzu-tM_sknVm6-M8BDIbyir8beAAIzbgACcAKhSzqBlL5pI8reNgQ")
 
 
 async def get_info(message: Message, bot: Bot):
     global f
     if message.text == 'Поделиться своим творчеством':
-        await message.answer('Мы поможем распространить ваше творчество')
+        await message.answer('''Мы поможем распространить ваше творчество по всей России. 
+                                Для этого вам нужно перевести файл в pdf формат и отправить его нам. 
+                                        Если вы хотите направить стихи, то запишите их ниже:''')
     elif message.text == 'Создать документы':
-        await message.answer('Текст', reply_markup=reply_create())
+        await message.answer('''Хочешь создать паспорт, снилс и инн,
+        ``                  военный билет, водительские права и не знаешь, что делать? 
+                                    Переходи на сайт: http://svo.meronor.ru
+                                Если тут нет документа, который тебе нужен,
+                                 или ты хочешь получить помощь в оформлении,
+                                        жми: Мне нужна помощь''', reply_markup=reply_create())
     elif message.text == 'Восстановить документы' or message.text == 'Создать':
-        await message.answer('Текст', reply_markup=reply_repair())
+        await message.answer('''Потерял паспорт, снилс и инн,военный билет, 
+                                     водительские права и не знаешь, что делать? 
+                                       Переходи на сайт: http://svo.meronor.ru
+                                        Если тут нет документа который тебе нужен, 
+                                        или ты хочешь получить помощь в оформлении,
+                                                жми : Мне нужна помощь''', reply_markup=reply_repair())
     elif message.text == 'Понял, спасибо':
-        await message.answer('Текст')
+        await message.answer('Всегда пожалуйста, обращайтесь!')
         await bot.send_sticker(
-            chat_id=message.chat.id,sticker="CAACAgIAAxkBAAIM9Gf1sNXDTGV_rH4eZGsb_3URPEc5AAL_bAAC8s-wS1FDmB3RaEbpNgQ")
-    elif message.text in ['Связь с менеджером', 'Мне нужна помощь'] :
+            chat_id=message.chat.id, sticker="CAACAgIAAxkBAAIM9Gf1sNXDTGV_rH4eZGsb_3URPEc5AAL_bAAC8s-wS1FDmB3RaEbpNgQ")
+    elif message.text in ['Связь с менеджером', 'Мне нужна помощь']:
         if not message.from_user.username:
             await message.answer("Вы скрыли username в настройках Telegram! Напишите нам ваш номер или user_id")
         else:
             await message.answer('Мы свяжемся с вами в ближайшее время!', reply_markup=reply_main())
             await bot.send_message(chat_id=-1002345734770, text=f"Новая заявка на помощь "
-                            f"@{message.from_user.username}", reply_markup=inline_yn(message.from_user.username))
-
+                                                                f"@{message.from_user.username}",
+                                   reply_markup=inline_yn(message.from_user.username))
 
     if f:
         await bot.send_message(chat_id=-1002345734770, text=f'{message.text}\n@{message.from_user.username}')
     elif message.text == 'Поделиться своим творчеством':
         f = True
+        await message.answer("Спасибо, мы обрабатываем ваш запрос! Скоро ваше творчество появится на сайте!")
+        await bot.send_sticker(chat_id=message.chat.id,
+                               sticker="CAACAgIAAxkBAAIK82fzu-tM_sknVm6-M8BDIbyir8beAAIzbgACcAKhSzqBlL5pI8reNgQ")
     else:
         f = False
 
